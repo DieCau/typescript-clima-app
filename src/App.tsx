96efa0bd2fbcd50523f5798b8dd7d26926200e4a
Diego Caucota <diegocaucota@gmail.com>
2024-07-23 01:27:02 -0300

    add: App.module.css and Css configurations

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
+import styles from "./App.module.css"
 
 function App() {
 
   return (
     <>
-      <h1>Buscador de Clima</h1>
+      <h1 className={styles.title}>Buscador de Clima</h1>
     </>
   )
 }
